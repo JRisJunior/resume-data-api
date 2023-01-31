dcfb83b9995a6d876755f8a71eabf281e1a5723c
johnryanmal <johnryanmal@gmail.com>
2023-01-31 13:38:16 -0800

    seed dump with id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_202610) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_212536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_202610) do
     t.string "photo_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_students_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,8 +72,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_202610) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "student_id"
-    t.index ["student_id"], name: "index_users_on_student_id"
   end
 
 end
