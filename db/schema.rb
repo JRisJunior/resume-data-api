08a32464addd437f04610bd9abff9e67ee09d1c8
johnryanmal <johnryanmal@gmail.com>
2023-01-31 10:22:35 -0800

    generate skill model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_182144) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_182221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_182144) do
     t.string "title"
     t.string "company"
     t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
