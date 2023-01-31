bd4f6d70a99381f24ba0d69cb79f4c38bc4eee57
johnryanmal <johnryanmal@gmail.com>
2023-01-31 12:07:04 -0800

    seed dump

---
+++ db/fake_seeds.rb
@@ -0,0 +1,19 @@
+5.times do #student
+	Student.create_fake!
+
+	2.times do #education per student
+		Education.create_fake!
+	end
+
+	3.times do #experience per student
+		Experience.create_fake!
+	end
+
+	5.times do #skills per student
+		Skill.create_fake!
+	end
+
+	2.times do #capstones per studnet
+		Capstone.create_fake!
+	end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,75 @@
-5.times do #student
-	Student.create_fake!
-
-	2.times do #education per student
-		Education.create_fake!
-	end
-
-	3.times do #experience per student
-		Experience.create_fake!
-	end
-
-	5.times do #skills per student
-		Skill.create_fake!
-	end
-
-	2.times do #capstones per studnet
-		Capstone.create_fake!
-	end
-end
+Student.create!([
+  {first_name: "Devin", last_name: "Rowe", email: "devin@example.org", phone: "1-171-504-2285", biography: "Est provident doloribus. Non labore quidem. Sed rerum soluta.", linkedin_url: nil, twitter_user: nil, website_url: nil, resume_url: nil, github_url: nil, photo_url: nil},
+  {first_name: "Laverna", last_name: "Robel", email: "laverna@example.org", phone: "1-363-766-1753", biography: "Voluptates et nesciunt. Qui suscipit nihil. Sit fugit eius.", linkedin_url: nil, twitter_user: nil, website_url: nil, resume_url: nil, github_url: nil, photo_url: nil},
+  {first_name: "Hermila", last_name: "Weissnat", email: "hermila@example.net", phone: "656.928.5051", biography: "Eius mollitia repudiandae. Nesciunt accusamus qui. Sunt aut unde.", linkedin_url: nil, twitter_user: nil, website_url: nil, resume_url: nil, github_url: nil, photo_url: nil},
+  {first_name: "Stacy", last_name: "Hills", email: "stacy@example.org", phone: "827-907-0477", biography: "Sapiente repellat rerum. Tempora consequatur similique. Voluptatem occaecati qui.", linkedin_url: nil, twitter_user: nil, website_url: nil, resume_url: nil, github_url: nil, photo_url: nil},
+  {first_name: "Dennis", last_name: "Huels", email: "dennis@example.org", phone: "307.671.3026", biography: "Nostrum repellendus ea. Dolorem explicabo quo. Veritatis nesciunt minus.", linkedin_url: nil, twitter_user: nil, website_url: nil, resume_url: nil, github_url: nil, photo_url: nil}
+])
+Skill.create!([
+  {name: "Teamwork"},
+  {name: "Confidence"},
+  {name: "Communication"},
+  {name: "Communication"},
+  {name: "Teamwork"},
+  {name: "Teamwork"},
+  {name: "Confidence"},
+  {name: "Confidence"},
+  {name: "Organisation"},
+  {name: "Confidence"},
+  {name: "Self-motivated"},
+  {name: "Organisation"},
+  {name: "Teamwork"},
+  {name: "Problem solving"},
+  {name: "Work under pressure"},
+  {name: "Proactive"},
+  {name: "Technical savvy"},
+  {name: "Self-motivated"},
+  {name: "Work under pressure"},
+  {name: "Problem solving"},
+  {name: "Networking skills"},
+  {name: "Organisation"},
+  {name: "Self-motivated"},
+  {name: "Work under pressure"},
+  {name: "Leadership"}
+])
+Experience.create!([
+  {start: "2022-03-03 00:00:00", end: "2023-10-14 00:00:00", title: "Customer Administrator", company: "Champlin-Bailey", details: "Quam quasi qui. Nihil eligendi labore. Aut explicabo molestiae. Ex unde voluptates. Consequuntur pariatur cum."},
+  {start: "2023-01-11 00:00:00", end: "2023-10-31 00:00:00", title: "Dynamic Executive", company: "Windler-Koss", details: "Qui laboriosam possimus. Excepturi quisquam et. Nemo deserunt sit. Aut enim consectetur. Inventore quasi tenetur."},
+  {start: "2022-04-02 00:00:00", end: "2023-03-10 00:00:00", title: "Product Administration Assistant", company: "Mosciski LLC", details: "Pariatur eveniet ipsa. Doloribus non accusantium. Optio iste nobis. Dolorem facilis tempore. Incidunt repellendus aliquam."},
+  {start: "2022-10-07 00:00:00", end: "2023-02-12 00:00:00", title: "Human Consulting Strategist", company: "Wilkinson, Spencer and Renner", details: "Nobis eum consectetur. Tempore qui ea. Maxime consectetur ut. Minus velit praesentium. Veniam molestiae excepturi."},
+  {start: "2022-09-20 00:00:00", end: "2023-03-12 00:00:00", title: "Retail Liaison", company: "Rohan LLC", details: "Voluptas necessitatibus eos. Qui exercitationem numquam. Vero quia dolor. Omnis et quibusdam. Tempore a aut."},
+  {start: "2023-01-13 00:00:00", end: "2023-04-19 00:00:00", title: "Healthcare Facilitator", company: "Jones-Brown", details: "Soluta rerum quos. Beatae aperiam aut. Itaque debitis quis. Eveniet tempore ab. Nesciunt fugit dignissimos."},
+  {start: "2022-06-20 00:00:00", end: "2023-09-17 00:00:00", title: "Dynamic Advertising Liaison", company: "West-Marvin", details: "Sapiente qui vero. Consequatur dolorem at. Ut ex facilis. Commodi ad dignissimos. Dolores saepe et."},
+  {start: "2022-09-22 00:00:00", end: "2023-07-29 00:00:00", title: "Banking Facilitator", company: "Cremin Group", details: "Nulla aliquam qui. Odio voluptate qui. Ut nam rerum. Sit adipisci sunt. Et commodi modi."},
+  {start: "2022-09-22 00:00:00", end: "2023-02-18 00:00:00", title: "District Banking Administrator", company: "Waelchi, Stehr and MacGyver", details: "Qui et nihil. Possimus libero et. Quibusdam sunt et. Tenetur perspiciatis rerum. Cupiditate dolor consequatur."},
+  {start: "2022-09-10 00:00:00", end: "2023-04-10 00:00:00", title: "Retail Engineer", company: "Dietrich-Keebler", details: "In nobis quos. Exercitationem velit libero. Animi rerum perspiciatis. Aliquid quis possimus. Voluptatem placeat itaque."},
+  {start: "2022-07-16 00:00:00", end: "2023-02-25 00:00:00", title: "International IT Consultant", company: "Schuppe, O'Conner and Ernser", details: "Maiores nemo impedit. Maxime aut aperiam. Corrupti reprehenderit qui. Dolorem natus quaerat. Consequuntur assumenda molestiae."},
+  {start: "2022-06-05 00:00:00", end: "2023-12-13 00:00:00", title: "Administration Facilitator", company: "Waelchi, Kuhn and Dicki", details: "Mollitia est qui. Ea voluptas delectus. Tempora repudiandae animi. Dicta voluptas nulla. Maxime consequatur maiores."},
+  {start: "2023-01-18 00:00:00", end: "2023-02-05 00:00:00", title: "Legacy IT Assistant", company: "Douglas, Blanda and Kozey", details: "Quia vel labore. Omnis vero officia. Delectus voluptas tempora. Ut consequuntur dolores. Harum ut eius."},
+  {start: "2022-03-12 00:00:00", end: "2023-08-25 00:00:00", title: "Regional Architect", company: "Mitchell-Kreiger", details: "Eveniet omnis officia. Laborum cupiditate qui. Quo quaerat sunt. Quo perferendis tempore. A qui assumenda."},
+  {start: "2022-12-18 00:00:00", end: "2023-07-11 00:00:00", title: "Product Hospitality Technician", company: "Lind and Sons", details: "Ut quibusdam nisi. Qui dolor facere. Assumenda est ut. Quod incidunt autem. Optio voluptatum dolor."}
+])
+Education.create!([
+  {start: "2022-05-29 00:00:00", end: "2024-01-05 00:00:00", degree: "Associate Degree in Medicine", university: "Bluemeadow College", details: "In dolorem ea. Eaque sint dolorem. Quod non consequatur. Veritatis possimus id. Modi voluptatem unde."},
+  {start: "2022-07-30 00:00:00", end: "2023-09-05 00:00:00", degree: "Bachelor of Biological Science", university: "Marblewald College", details: "Dolorum aperiam id. Sit laudantium repellat. Ut consectetur doloribus. Sit nam fugiat. Eligendi omnis voluptatum."},
+  {start: "2023-01-29 00:00:00", end: "2023-10-13 00:00:00", degree: "Bachelor of Arts", university: "Marblewald College", details: "Consequatur aut voluptatem. Quis illo aut. Temporibus aspernatur sint. Cumque odit saepe. Sapiente qui quis."},
+  {start: "2022-09-10 00:00:00", end: "2023-12-08 00:00:00", degree: "Bachelor of Communications", university: "Mallowpond University", details: "Eos quae laborum. Et accusamus accusantium. Et quasi aliquam. Fugiat sunt est. Eum ut possimus."},
+  {start: "2022-11-22 00:00:00", end: "2023-08-19 00:00:00", degree: "Associate Degree in Design", university: "Brighthurst University", details: "Quia omnis quia. Mollitia modi ad. Dolor reprehenderit et. Aliquam sequi dolore. Omnis numquam est."},
+  {start: "2022-07-26 00:00:00", end: "2023-12-28 00:00:00", degree: "Associate Degree in Architectural Technology", university: "Brookville University", details: "At qui facere. Vero provident aut. Quo reiciendis expedita. Porro alias in. Deleniti possimus non."},
+  {start: "2022-06-06 00:00:00", end: "2023-12-27 00:00:00", degree: "Master of Computer Science", university: "Icelyn TAFE", details: "Eaque maiores corporis. Porro quam voluptatum. Molestiae magni dicta. Harum neque nihil. Qui cum voluptatem."},
+  {start: "2022-10-13 00:00:00", end: "2023-05-11 00:00:00", degree: "Master of Teaching", university: "Vertapple Technical College", details: "Vero officiis praesentium. Aut recusandae eligendi. Asperiores quisquam esse. In sed itaque. Deleniti illo et."},
+  {start: "2022-06-03 00:00:00", end: "2023-06-14 00:00:00", degree: "Bachelor of Biological Science", university: "Brookville College", details: "Magni optio quidem. Sapiente alias adipisci. Sequi quidem vel. Quo cum inventore. Maiores rerum dolorum."},
+  {start: "2022-10-22 00:00:00", end: "2023-03-01 00:00:00", degree: "Associate Degree in Medicine", university: "Lakeacre Technical College", details: "Qui soluta non. Molestiae enim fugit. Odit praesentium id. In et aut. Libero quasi est."}
+])
+Capstone.create!([
+  {name: "Ronstring", description: "Illum dolorum quia. Minima eos enim. Voluptatum consequuntur consequatur. In consequatur quis.", url: "http://example.com/karisa.okuneva"},
+  {name: "Ronstring", description: "Et cupiditate cumque. Molestias consequatur eligendi. Qui officia aut. Mollitia et maxime.", url: "http://example.com/murray.romaguera"},
+  {name: "Namfix", description: "Rerum et delectus. Nesciunt dolores iste. Eum rerum qui. Sapiente qui perferendis.", url: "http://example.com/jackson"},
+  {name: "Alphazap", description: "Voluptas harum modi. Laudantium cupiditate aut. Voluptas ad dolorum. Et officiis nulla.", url: "http://example.com/luke"},
+  {name: "Sonsing", description: "Harum non culpa. Harum at aspernatur. Dolor ad excepturi. Quas libero et.", url: "http://example.com/alvaro_schuster"},
+  {name: "Ventosanzap", description: "Id veritatis ut. Numquam magnam quis. Atque esse repudiandae. Voluptatem fugiat veniam.", url: "http://example.com/terence"},
+  {name: "Zontrax", description: "Beatae ab quae. Ea incidunt et. Voluptate molestiae sed. Ipsa sapiente aperiam.", url: "http://example.com/sammy_corwin"},
+  {name: "Toughjoyfax", description: "Non quia omnis. Aut molestiae voluptas. Voluptas occaecati sapiente. Consequatur dolore voluptatum.", url: "http://example.com/zita_mcdermott"},
+  {name: "Stim", description: "Voluptatibus maxime unde. Ut fugit atque. Mollitia illum tempore. Consequatur vel perferendis.", url: "http://example.com/elina_hammes"},
+  {name: "Veribet", description: "Ea adipisci in. Et repellat quia. Aliquam atque et. Officiis pariatur non.", url: "http://example.com/hue.graham"}
+])
