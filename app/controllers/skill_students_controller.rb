1fec2775f1ef257defdf5a0be5c1ae766ce2b4b9
johnryanmal <johnryanmal@gmail.com>
2023-02-02 11:40:39 -0800

    generate skill student controller

---
+++ app/controllers/skill_students_controller.rb
@@ -0,0 +1,34 @@
+class SkillStudentsController < ApplicationController
+	def index
+    skill = Skill.all
+    render json: skill.as_json
+  end
+
+  def show
+    skill = Skill.find_by(id: params[:id])
+    render json: skill.as_json
+  end
+
+  def destroy
+    skill = Skill.find_by(id: params[:id])
+    skill.delete
+    render json: {message: "Successfully deleted"}
+  end
+
+  def create
+    skill = Skill.create!(
+      name: params[:name]
+    )
+    skill.save
+    render json: {message: "Successfully Created"}
+  end
+
+  def update
+    skill = Skill.find_by(id: params[:id])
+    skill.update(
+      name: params[:name]
+    )
+    skill.save
+    render json: {message: "Successfully updated"}
+  end
+end
